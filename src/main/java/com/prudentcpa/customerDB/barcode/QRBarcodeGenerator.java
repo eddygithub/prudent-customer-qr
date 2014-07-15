7a592c5aa468c917729b36be7bd45d066160d764
Eddy Zhu <eddygithub@gmail.com>
2014-07-15 12:48:48 -0400

    removed not used import

--- src/main/java/com/prudentcpa/customerDB/barcode/QRBarcodeGenerator.java
+++ src/main/java/com/prudentcpa/customerDB/barcode/QRBarcodeGenerator.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
